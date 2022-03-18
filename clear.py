2bb18bf3d0264fd25e8e7ae9f436ba75ba95d97d
Jacob Bolda <me@jacobbolda.com>
2022-03-18 14:08:19 -0500

    rough out server

---
+++ clear.py
@@ -0,0 +1,16 @@
+import time
+
+from inky.auto import auto
+from inky.inky_uc8159 import CLEAN
+
+inky = auto(ask_user=True, verbose=True)
+
+
+def clear():
+    for _ in range(2):
+        for y in range(inky.height - 1):
+            for x in range(inky.width - 1):
+                inky.set_pixel(x, y, CLEAN)
+
+        inky.show()
+        time.sleep(1.0)
